662c001cb226a22bf33ddc9e42245699d3061196
Christian Lienen <christianlienen@me.com>
2020-06-17 10:39:41 +0200

    sobel filter for ros works now

--- Sobel/src/rt_sobel/c/rt_sobel.c
+++ Sobel/src/rt_sobel/c/rt_sobel.c
@@ -16,13 +16,13 @@
 #include <stdio.h>
 
 
-#define INPUT_WIDTH 640
+#define INPUT_WIDTH 480 // because of the rgb8 format -> 24 bit per pixel
 #define INPUT_HEIGHT 480
 #define INPUT_LINEBUFFER_SIZE (INPUT_WIDTH * 4 * 4)
 #define INPUT_PREFETCH_SIZE	  (INPUT_WIDTH * 4 * 2)
 #define INPUT_LINESIZE (INPUT_WIDTH * 4)
 #define OUTPUT_LINEBUFFER_SIZE (INPUT_WIDTH * 4)
-#define OUTPUT_WIDTH 1680
+#define OUTPUT_WIDTH INPUT_WIDTH
 #define OUTPUT_LINE_SIZE (OUTPUT_WIDTH * 4 )
 
 #define MEM_READ_L( adr, dest, length ) { memcpy( dest, (void*)adr,  length); }
@@ -33,23 +33,29 @@ const int filter_x[] = { 1,  2,  1,  0,  0,  0, -1, -2, -1};
 const int filter_y[] = { 1,  0, -1,  2,  0, -2,  1,  0, -1};
 
 
+
+
 THREAD_ENTRY()
 {
 	THREAD_INIT();
 
-	struct reconos_thread * rt = GET_INIT_DATA();
-
-	uint32_t fb = (uint32_t)rt->init_data;
+	uint32_t bStart = 1;
 
 	uint32_t input_linebuffer[INPUT_LINEBUFFER_SIZE/4];
 	uint32_t output_linebuffer[OUTPUT_LINEBUFFER_SIZE/4];
 	int32_t i,k,j, ii, jj;
-	int16_t tmp_x[3], tmp_y[3];
+	int16_t tmp_x[4], tmp_y[4];
 	uint8_t filter_pointer;
+
+	//uint8_t * outputdata = malloc(640*480*4);
 
 	while (1)
 	{
 		ROS_SUBSCRIBE_TAKE(video_subdata, video_image_msg);
+
+		printf("%d %d %d \n",   	video_image_msg->data.data,	video_image_msg->data.size,	video_image_msg->data.capacity);
+
+		printf("Got image, compute .. \n");
 		uint32_t address = (uint32_t)video_image_msg->data.data;
 		
 		MEM_READ_L( address, input_linebuffer, INPUT_PREFETCH_SIZE);
@@ -66,6 +72,7 @@ THREAD_ENTRY()
 				tmp_x[0]= 0; tmp_y[0] = 0;
 				tmp_x[1]= 0; tmp_y[1] = 0;
 				tmp_x[2]= 0; tmp_y[2] = 0;
+				tmp_x[3]= 0; tmp_y[3] = 0;
 
 				filter_pointer = 0;
 				for(ii=-1; ii < 2; ii++)
@@ -74,7 +81,7 @@ THREAD_ENTRY()
 					{		
 						uint32_t buffer_pointer = ((INPUT_WIDTH*((i+ii)&3)+(j+jj)));
 						uint32_t actindata  = 	input_linebuffer[buffer_pointer];	
-						for(k = 0; k < 3; k++)
+						for(k = 0; k < 4; k++)
 						{
 							int16_t data = ((actindata >> 8*k) & 0x000000ff);
 							tmp_x[k] += data * filter_x[filter_pointer];
@@ -84,14 +91,20 @@ THREAD_ENTRY()
 						filter_pointer++;
 					}	
 				}
-				output_linebuffer[(j)] = 0xff000000 | (((abs(tmp_x[0]) + abs(tmp_y[0])) >> 3)) | (((abs(tmp_x[1]) + abs(tmp_y[1])) >> 3) << 8) | (((abs(tmp_x[2]) + abs(tmp_y[2])) >> 3) << 16);
+				output_linebuffer[(j)] = (((abs(tmp_x[0]) + abs(tmp_y[0])) >> 3)) | (((abs(tmp_x[1]) + abs(tmp_y[1])) >> 3) << 8) | (((abs(tmp_x[2]) + abs(tmp_y[2])) >> 3) << 16) | (((abs(tmp_x[3]) + abs(tmp_y[3])) >> 3) << 24);
 			}
 			
-			MEM_WRITE_L( output_linebuffer , (video_image_msg->data.data + i* OUTPUT_LINE_SIZE), INPUT_LINESIZE);
+			//uint32_t sum = 0;
+			//for(int i = 0; i < INPUT_LINESIZE/4; i++)
+			//sum+= ((uint8_t*)outputdata + i* OUTPUT_LINE_SIZE)[i];
+
+			//printf("sum : %d \n", sum);
+
+			MEM_WRITE_L( output_linebuffer , ((uint8_t*)video_image_msg_out->data.data + i* OUTPUT_LINE_SIZE), INPUT_LINESIZE);
 			
 		}
 		
-		ROS_PUBLISH(video_pubdata, video_image_msg);
+		ROS_PUBLISH(video_pubdata, video_image_msg_out);
 
 
 
